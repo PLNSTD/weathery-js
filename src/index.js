0f9b959639bcfc3cff02d768554336df915da54e
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-03-03 14:30:57 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
 import './style.css';
 // import icon from './icon.png'; To load some image for the code
 
-for (let i = 0; i < 3; i += 1) {
-  console.log(i);
+const baseURL = 'http://api.weatherapi.com/v1';
+const key = 'de5b8821295a445cb73200758240203';
+
+const getWeather = async (city) => {
+  const request = await fetch(`${baseURL}/current.json?key=${key}&q=${city}`);
+
+  const response = await request.json();
+
+  console.log(response);
+
+  return response;
+}
+
+const cardCreation = async (cityToGet) => {
+
+  const data = await getWeather(cityToGet).catch((err) => {
+    console.log(err);
+  });
+
+  const city = data.location;
+  const weather = data.current.condition;
+
+  // INFO CITY TIME  
+  // const cityName = city.name;
+  const cityTime = city.localtime.slice(11);
+  const cityDate = city.localtime.slice(0, 10);
+  
+  // const cityNameElement = document.getElementById('city-paragraph');
+  const cityTimeElement = document.getElementById('user-time-paragraph');
+  const cityDateElement = document.getElementById('user-date-paragraph');
+
+  // cityNameElement.innerHTML = cityName;
+  cityTimeElement.innerHTML = cityTime;
+  cityDateElement.innerHTML = cityDate;
+
+  // INFO CITY WEATHER
+  const weatherIconElement = document.getElementById('weather-icon');
+  const weatherDescElement = document.getElementById('weather-desc');
+
+  weatherIconElement.src = weather.icon;
+  weatherDescElement.innerHTML = weather.text;
+};
+
+const cardReset = () => {
+  const cityTimeElement = document.getElementById('user-time-paragraph');
+  const cityDateElement = document.getElementById('user-date-paragraph');
+  const weatherIconElement = document.getElementById('weather-icon');
+  const weatherDescElement = document.getElementById('weather-desc');
+
+  cityTimeElement.innerHTML = 'HH:MM';
+  cityDateElement.innerHTML = 'Date';
+  weatherIconElement.src = '/weathery-js/src/asset/weather-forecast.png';
+  weatherDescElement.innerHTML = 'No city';
+}
+
+const dropdownList = document.getElementById('cities');
+dropdownList.onchange = () => {
+  if( dropdownList.value !== 'none') cardCreation(dropdownList.value);
+  else cardReset();
 }
